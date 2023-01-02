738579e95dcd3c4404d1216618c1a6ac2063f4ef
Rob Osborne <ozborne132@gmail.com>
2023-01-02 21:04:01 +0000

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CPPND: Capstone Hello World Repo
+# CPPND: Capstone Repo
 
 This is a starter repo for the Capstone project in the [Udacity C++ Nanodegree Program](https://www.udacity.com/course/c-plus-plus-nanodegree--nd213).
 
@@ -23,4 +23,11 @@ In this project, you can build your own C++ application starting with this repo,
 1. Clone this repo.
 2. Make a build directory in the top level directory: `mkdir build && cd build`
 3. Compile: `cmake .. && make`
-4. Run it: `./HelloWorld`.
+4. Run it: `./Capstone > image.ppm`.
+
+## Project Scripts
+
+Run the following scripts to easily perform project actions.
+```bash
+./main.sh               // Starts project from build.
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << "\n";
-    return 0;
+    const int imageWidth = 256;
+    const int imageHeight = 256;
+    const double rgbValue = 255.999;
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int j = imageHeight - 1; j >= 0; --j) {
+        for (int i = 0; i < imageWidth; ++i) {
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeight - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(rgbValue * r);
+            int ig = static_cast<int>(rgbValue * g);
+            int ib = static_cast<int>(rgbValue * b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 }
